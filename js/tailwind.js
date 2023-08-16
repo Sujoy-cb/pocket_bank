608556eb758fdf557d4edaea30502382a8c1df1a
Sujoy chakraborty <sujoyc070@gmail.com>
2023-08-16 21:45:36 +0600

    nav

--- js/tailwind.js
+++ js/tailwind.js
@@ -1,6 +1,9 @@
 tailwind.config = {
     theme: {
       extend: {
+        fontFamily: {
+          roboto: ['Roboto', 'sans-serif'],
+        },
         colors: {
           clifford: '#da373d',
         }
