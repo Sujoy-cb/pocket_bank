17600cc125d92f9d7948c0987e9db48149955cb3
Sujoy chakraborty <sujoyc070@gmail.com>
2023-08-17 22:53:57 +0600

    cash done

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ let dropbtn = document.querySelector('.name')
 dropbtn.addEventListener('click',()=>{
     drop.classList.toggle('hidden')
 })
+
+// Active Class//
+
+let activeList = document.querySelectorAll('.nav-item')
+
+activeList.forEach((item)=>{
+    item.addEventListener('click',()=>{
+        activeList.forEach((item)=>{
+            item.classList.remove('active')
+        })
+        item.classList.add('active')
+    })
+})
