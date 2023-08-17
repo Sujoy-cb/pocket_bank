2d5dd83bc19358f1ab246f4b33e68f44d36f569c
Sujoy chakraborty <sujoyc070@gmail.com>
2023-08-17 11:26:39 +0600

    nav done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Porfile Drop Down //
+let drop = document.querySelector('.drop')
+
+let dropbtn = document.querySelector('.name')
+
+dropbtn.addEventListener('click',()=>{
+    drop.classList.toggle('hidden')
+})
